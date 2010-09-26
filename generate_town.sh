fa863abc3e4032e8154b721b3a57ed345e6d96c2
fanioz <fanio.zilla@gmail.com>
2010-09-26 14:08:50 +0700

    [NML] -Changed: Splitted script for generating and counting

--- generate_town.sh
+++ generate_town.sh
@@ -7,8 +7,6 @@ CITIES="big x00 x01"
 #number of city file + 1
 let N_CITIES=4;
 
-cat sprites/header.pnml > $NML
-
 for i in $CITIES; do
 cat >>$NML <<EOF
 town_names($i) {
@@ -20,16 +18,10 @@ done
 
 cat sprites/mother.pnml >> $NML
 
-let NUM_ALL_CITIES=0
 for i in $CITIES; do
   let --N_CITIES
   echo "    town_names($i, $N_CITIES)," >>$NML
-  let NUM_CUR_CITY=`cat cities/$i.txt | wc -l`
-  echo number of generated towns from $i.txt was $NUM_CUR_CITY
-  let NUM_ALL_CITIES+=NUM_CUR_CITY
 done
 
 cat sprites/closebracket.pnml >> $NML
-echo Total town generated: $NUM_ALL_CITIES
-echo NUMTOWNS: $NUM_ALL_CITIES >  number.txt
 exit 0
